e2c763921630c5253bd479b64f9da9b6964ce808
edwar <edwar@DESKTOP-3GRP4BC>
2017-04-21 03:04:56 +0700

    Added input from file

--- src/toolboothsimulation/TollBooth.java
+++ src/toolboothsimulation/TollBooth.java
@@ -8,6 +8,21 @@ public class TollBooth {
     private TollBoothLine line;
     private DoneVehicles doneLine;
     private int maxLength;
+    private static double timeUntilAvailable;
+    public static void tick()//one second has passed
+    {
+        timeUntilAvailable--; //it's ok to be negative
+    }
+    public static void setTimeUntilAvailable(double time) //set the delay time after a vehicle is serviced (1 for automatic and numWheel*1 for manual)
+    {
+        if (time>=0) {
+            timeUntilAvailable=time;
+        }
+    }
+    public static boolean isAvailable() //see is there is another car still in the tollbooth
+    {
+        return timeUntilAvailable==0;
+    }
     public TollBooth(){
         line=new TollBoothLine();
         doneLine=new DoneVehicles();
