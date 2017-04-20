b67f130411e8935ab82782272c21338ddb49a5a3
edwar <edwar@DESKTOP-3GRP4BC>
2017-04-21 03:04:40 +0700

    Added input from file

--- src/toolboothsimulation/ToolBoothSimulation.java
+++ src/toolboothsimulation/ToolBoothSimulation.java
@@ -1,24 +1,26 @@
 package toolboothsimulation;
+
 import java.util.*;
 import java.io.*;
+
 public class ToolBoothSimulation {
+
     public static void main(String[] args) throws FileNotFoundException {
+
         TollBoothLine allVehicle = new TollBoothLine();
         String filePath = "rushhour.txt";
         Scanner scan = new Scanner(new File(filePath));
-        while(scan.hasNext())
-        {
-            StringTokenizer elements = new StringTokenizer(scan.nextLine(),",");
+        while (scan.hasNext()) {
+            StringTokenizer elements = new StringTokenizer(scan.nextLine(), ",");
             Double arrivalTime = Double.parseDouble(elements.nextToken());
             String type = elements.nextToken();
             if (type.toLowerCase().equals("m")) {
                 int numWheel = Integer.parseInt(elements.nextToken());
                 allVehicle.addLast(new ManuVehicle(arrivalTime, numWheel));
-            }
-            else{
+            } else {
                 allVehicle.addLast(new AutoVehicle(arrivalTime));
             }
+            System.out.println(allVehicle);
         }
-        System.out.println(allVehicle);
-    }
+    }   
 }
